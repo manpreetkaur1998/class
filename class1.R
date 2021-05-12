3fbf68f37dd59680a4387dece59280d40958c7cd
unknown <mkaur2563@gmail.com>
2021-05-12 23:08:35 +0530

    bioinfomatics2

---
+++ class1.R
@@ -0,0 +1,108 @@
+# to find what is there in number
+b= (c(12,13,45,78))
+b
+str # mean structure
+str(b)
+class(b)
+typeof(b)
+?double # integer+ decimal
+k=(c(34,56,78,90))
+k
+str(k)
+class(k)
+typeof(k)
+# to present in the form of strings
+#string= you are storing your value in special character
+a="g"
+a
+a="x1"
+a
+# x1 it could be anything
+# patient name
+# student name
+# to convert integer integer into numbers
+a=(c(34.4,56.4,67.8,78.9))
+a
+as.integer(a)
+a=(c(43.6,78.9,45.2,12.7))
+a
+as.integer(a)
+round(a)
+str(a)
+# to convert integer into numbers
+h=(c(23.3,56.5,67.8,98.5))
+as.integer(h)
+round(h)
+str(h)
+str(as.integer(h))
+str(round(h))
+class(h)
+typeof(h)
+a=1:10
+a
+a[3]
+a[4:5]
+a=2:20
+a
+a[6]
+a[7:9]
+a[3:9]
+a=4:40
+a
+a[5:25]
+a[c(3,5,7)] #alternative way to find out in a list
+a[c(4,8,4)]
+str(a)
+class(a)
+typeof(a)
+a=(c("12","34","78","90"))
+a   
+sum(a)
+as.numeric(a)
+sum(as.numeric(a))
+j=(c("34","56","78","76"))
+j
+sum(j)
+as.numeric(j)
+sum(as.numeric(j))
+str(j)
+class(j)
+typeof(j)
+#true/flase
+is.numeric(j)
+is.character(j)
+is.integer(j)
+is.numeric(j)
+as.numeric(j)
+j
+is.numeric(j)
+is.character(j)
+is.integer(j)
+is.vector(j)
+is.list(j)
+a=(c("34","45","67","78"))
+a
+a=as.numeric(a)
+a
+sum(as.numeric(a))
+is.character(a)
+a=as.character(a)
+is.character(a)
+is.numeric(a)
+a=as.numeric(a)
+is.numeric(a)
+a=as.character(a)
+is.character(a)
+is.vector(a)
+is.list(a)
+a=as.list(a)
+is.list(a)
+a
+s=(c("23","45","67","90"))
+is.list(s)
+s=as.list(s) #this is how you create a list
+is.list(s)
+s
+list(c(1,2,3),c(4,5,6))
+list(c(34,56,78),c(23,56,78),c(56,87,89))
+list(c(34,45),c(89,67,89))
